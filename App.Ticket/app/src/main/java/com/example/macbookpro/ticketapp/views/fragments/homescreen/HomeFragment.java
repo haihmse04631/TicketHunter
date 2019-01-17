862990ff418e8b5ba8bdfcd5409ab69c959051fd
hongsongp97 <hongsongp97@gmail.com>
2019-01-18 04:54:21 +0700

    demo get information of user

--- App.Ticket/app/src/main/java/com/example/macbookpro/ticketapp/views/fragments/homescreen/HomeFragment.java
+++ App.Ticket/app/src/main/java/com/example/macbookpro/ticketapp/views/fragments/homescreen/HomeFragment.java
@@ -6,16 +6,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.macbookpro.ticketapp.R;
 import com.example.macbookpro.ticketapp.databinding.FragmentHomeBinding;
 import com.example.macbookpro.ticketapp.views.base.BindingFragment;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.login.LoginResult;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,31 +50,63 @@ public class HomeFragment extends BindingFragment {
         super.onViewCreated(view, savedInstanceState);
         binding = (FragmentHomeBinding) getViewBinding();
 
+        loadInformation();
+
         callbackManager = CallbackManager.Factory.create();
 
-        binding.loginButton.setReadPermissions("email");
+        binding.loginButton.setReadPermissions(Arrays.asList("email"));
         binding.loginButton.setFragment(this);
 
         // Callback registration
         binding.loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                // App code
                 Toast.makeText(getContext(), loginResult.getAccessToken().getUserId() + " logined!", Toast.LENGTH_SHORT).show();
+                loadInformation();
             }
 
             @Override
             public void onCancel() {
-                // App code
                 Toast.makeText(getContext(), "canceled", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(FacebookException exception) {
-                // App code
                 Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    protected void loadInformation() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+        if (isLoggedIn) {
+            Bundle params = new Bundle();
+            params.putString("fields", "id,name,email,picture.width(960)");
+
+            new GraphRequest(AccessToken.getCurrentAccessToken(),
+                "/" + accessToken.getUserId() + "/",
+                params,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        Log.i("Data: ", response.toString());
+                        try {
+                            JSONObject obj = response.getJSONObject();
+                            binding.textView.setText(obj.getString("name"));
+                            binding.textView2.setText(obj.getString("email"));
+                            Picasso.get().load(obj.getJSONObject("picture").getJSONObject("data")
+                                .getString("url")).into(binding.imageView);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            ).executeAsync();
+        } else {
+            Log.i("Data: ", "Not yet");
+        }
     }
 
     @Override
