ce6d13b6695f5336a4eecc0e0f9e1a3fc79a0268
hongsongp97 <hongsongp97@gmail.com>
2019-01-18 00:37:38 +0700

    Complete to demo login via FB

--- App.Ticket/app/src/main/java/com/example/macbookpro/ticketapp/views/fragments/homescreen/HomeFragment.java
+++ App.Ticket/app/src/main/java/com/example/macbookpro/ticketapp/views/fragments/homescreen/HomeFragment.java
@@ -1,15 +1,21 @@
 package com.example.macbookpro.ticketapp.views.fragments.homescreen;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.macbookpro.ticketapp.R;
 import com.example.macbookpro.ticketapp.databinding.FragmentHomeBinding;
 import com.example.macbookpro.ticketapp.views.base.BindingFragment;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +23,7 @@ import com.example.macbookpro.ticketapp.views.base.BindingFragment;
 public class HomeFragment extends BindingFragment {
 
     private FragmentHomeBinding binding;
+    private CallbackManager callbackManager;
 
     public static HomeFragment newInstance() {
 
@@ -32,6 +39,31 @@ public class HomeFragment extends BindingFragment {
         super.onViewCreated(view, savedInstanceState);
         binding = (FragmentHomeBinding) getViewBinding();
 
+        callbackManager = CallbackManager.Factory.create();
+
+        binding.loginButton.setReadPermissions("email");
+        binding.loginButton.setFragment(this);
+
+        // Callback registration
+        binding.loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Toast.makeText(getContext(), loginResult.getAccessToken().getUserId() + " logined!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Toast.makeText(getContext(), "canceled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -42,5 +74,11 @@ public class HomeFragment extends BindingFragment {
     @Override
     protected int containerViewId() {
         return R.id.navigation_home;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
