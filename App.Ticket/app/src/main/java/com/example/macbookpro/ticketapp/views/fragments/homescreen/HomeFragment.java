5cca6a405df781437ad48dc942d708c953da1e37
HoangHai <hoangminhhai1997@gmail.com>
2019-01-17 23:16:58 +0700

    add binding instance in home fragment

--- App.Ticket/app/src/main/java/com/example/macbookpro/ticketapp/views/fragments/homescreen/HomeFragment.java
+++ App.Ticket/app/src/main/java/com/example/macbookpro/ticketapp/views/fragments/homescreen/HomeFragment.java
@@ -2,15 +2,21 @@ package com.example.macbookpro.ticketapp.views.fragments.homescreen;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
 import com.example.macbookpro.ticketapp.R;
+import com.example.macbookpro.ticketapp.databinding.FragmentHomeBinding;
 import com.example.macbookpro.ticketapp.views.base.BindingFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends BindingFragment {
+
+    private FragmentHomeBinding binding;
 
     public static HomeFragment newInstance() {
 
@@ -19,6 +25,13 @@ public class HomeFragment extends BindingFragment {
         HomeFragment fragment = new HomeFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = (FragmentHomeBinding) getViewBinding();
+
     }
 
     @Override
